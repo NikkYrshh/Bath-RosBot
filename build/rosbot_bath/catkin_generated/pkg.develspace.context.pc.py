42d6c5cefd82b94853dd9b434662cb401f60f92f
NikkYrshh <niki.iersh@gmail.com>
2023-11-30 22:26:44 +0000

    minor changes

--- build/rosbot_bath/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "rosbot_bath"
-PROJECT_SPACE_DIR = "/home/nikk_yrsh/workspaces/rosbot_ws/devel"
-PROJECT_VERSION = "0.0.0"
