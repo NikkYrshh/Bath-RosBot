777c31e45587f959359dc6000c31aada8d985a05
NikkYrshh <niki.iersh@gmail.com>
2023-11-30 16:50:51 +0000

    project init first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nikk_yrsh/workspaces/rosbot_ws/src
+# Build directory: /home/nikk_yrsh/workspaces/rosbot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rosbot_bath")
---
+++ build/rosbot_bath/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nikk_yrsh/workspaces/rosbot_ws/src/rosbot_bath
+# Build directory: /home/nikk_yrsh/workspaces/rosbot_ws/build/rosbot_bath
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
