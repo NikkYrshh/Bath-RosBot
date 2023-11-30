8573f8977c9c4cb6a4c9f0d62a92d29b6dbe05d6
NikkYrshh <niki.iersh@gmail.com>
2023-11-30 21:10:58 +0000

    added header of rosbot class

---
+++ src/rosbot_bath/include/rosbot_bath/rosbot_class.h
@@ -0,0 +1,54 @@
+#ifndef ROSBOT_CLASS_H
+#define ROSBOT_CLASS_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int16.h"
+
+class ROSbot {
+ private:
+  std::unique_ptr<ros::NodeHandle> nh;
+  
+  std::vector<float> laser_range;
+  // subscribers
+  ros::Subscriber laser_sub;
+  ros::Subscriber odom_sub;
+  // velocity pub and msg
+  ros::Publisher vel_pub;
+  geometry_msgs::Twist vel_msg;
+
+  // topics
+  std::string laser_topic;
+  std::string vel_topic;
+  std::string odom_topic;
+
+  // pose x,y,z
+  float pos_x;
+  float pos_y;
+  float pos_z;
+
+  void laser_callback(const sensor_msgs::LaserScan::ConstPtr &laser_msg);
+  void odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg);
+
+ public:
+  ROSbot();
+  void move();
+  void move_forward(int time);
+  void move_backwards(int time);
+  void turn(std::string clock, int time);
+  void stop();
+  float get_position(int param);
+  std::list<float> get_position_full();
+  double get_time();
+  float get_laser(int index);
+  float *get_laser_full();
+};
+
+#endif  // ROSBOT_CLASS_H
---
+++ src/rosbot_bath/src/rosbot_class.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+#include "rosbot_bath/rosbot_class.h"
+
+#include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
+#include "sensor_msgs/LaserScan.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int16.h"
+#include "ros/ros.h"
+// #include <opencv2/opencv.hpp>
+
